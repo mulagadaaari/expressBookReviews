eeea09ece12a90fb3d972990b5455691b48047a4
mulagadaaari <mailaari.m@gmail.com>
2026-05-31 07:33:45 +0530

    Completed Task 7 - User login with JWT

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -51,8 +51,36 @@ regd_users.post("/register", (req, res) => {
 
 // only registered users can login
 regd_users.post("/login", (req, res) => {
-  return res.status(300).json({
-    message: "Yet to be implemented"
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    return res.status(404).json({
+      message: "Error logging in"
+    });
+  }
+
+  if (authenticatedUser(username, password)) {
+
+    let accessToken = jwt.sign(
+      {
+        data: password
+      },
+      'access',
+      { expiresIn: 60 * 60 }
+    );
+
+    req.session.authorization = {
+      accessToken,
+      username
+    };
+
+    return res.status(200).send("Customer successfully logged in.");
+  }
+
+  return res.status(208).json({
+    message: "Invalid Login. Check username and password"
   });
 });
 
