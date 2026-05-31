1e1ee0702b876f6509bccc9f91f523971b6e8b87
mulagadaaari <mailaari.m@gmail.com>
2026-05-31 06:11:06 +0530

    Completed Task 5 - Get book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -47,10 +47,9 @@ public_users.get('/title/:title',function (req, res) {
   return res.status(200).json(result);
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+  const isbn = req.params.isbn;
+  return res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
 });
 
 module.exports.general = public_users;
