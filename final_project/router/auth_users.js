5e8c257bf1b27e249c82298d1ddb96b2c7cc2fd1
mulagadaaari <mailaari.m@gmail.com>
2026-05-31 07:55:34 +0530

    Completed Task 8 - Add or modify review

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -84,10 +84,17 @@ regd_users.post("/login", (req, res) => {
   });
 });
 
-// Add a book review
+// Add or Modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  return res.status(300).json({
-    message: "Yet to be implemented"
+
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "The review has been added/updated successfully"
   });
 });
 
